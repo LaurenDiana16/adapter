2df8f2b240aa0e11a38e40ba79dfcaaa4e96c883
Lauren Gallic <ldwilli5@ncsu.edu>
2025-09-19 19:47:14 -0400

    cleaned up code

--- certifier_agent/update_agent_facts.py
+++ certifier_agent/update_agent_facts.py
@@ -2,31 +2,51 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from pymongo import MongoClient
+import json
 
-# Pretend this agent needs to get a certification added for their skill
-agent_id="ENTERPRISE_13"
+def update_agent_facts(agent_name):
+    """Function that adds a certification to a AgentFacts record in the MongoDB database."""
 
-# Connect to MongoDB
-client = MongoClient('')'
-db = client.agent_facts  # Replace with your database name
-collection = db.facts  # Replace with your collection name
+    # Connect to MongoDB
+    client = MongoClient('your-connection-string') # Replace with your connection string
+    db = client.Agents  # Replace with your database name
+    collection = db.AgentFacts # Replace with your collection name
 
-# Define the query to find the document
-myquery = { "id": agent_id }
+    # Define the query to find the document
+    myquery = {"agent_name": agent_name}
 
-# Get today's date and calculate one year forward
-today = date.today()
-today_string = today.strftime("%Y-%m-%d")
-one_year = today + relativedelta(years=1)
-one_year_string = one_year.strftime("%Y-%m-%d")
+    # Get today's date and calculate one year forward
+    today = date.today()
+    today_string = today.strftime("%Y-%m-%d")
+    one_year = today + relativedelta(years=1)
+    one_year_string = one_year.strftime("%Y-%m-%d")
 
-# Define the new values using the $set operator
-newvalues = { "$set": { "certification": 
-                        {"level": "certified",
-                        "issuer": "NANDA Certification Authority",
-                        "issuanceDate": today_string,
-                        "expirationDate": one_year_string,
-                        "_id":""}}}
+    # Define the new values using the $set operator
+    newvalues = { "$set": { "certification": 
+                            {"level": "certified",
+                            "issuer": "NANDA Certification Authority",
+                            "issuanceDate": today_string,
+                            "expirationDate": one_year_string,
+                            "_id":""}}}
 
-# Perform the update
-collection.update_one(myquery, newvalues)
+    # Perform the update
+    collection.update_one(myquery, newvalues)
+
+    return
+
+def load_file(file_path):
+    """Load the certifications JSON file."""
+    
+    with open(file_path, 'r') as f:
+        data_list = json.load(f)
+
+    return data_list
+
+
+# Load in certifications JSON file
+file_path = "certifications.jsonl"
+data_list = load_file(file_path)
+
+# Update each agent
+for item in data_list:
+    update_agent_facts(item['agent_name'])
