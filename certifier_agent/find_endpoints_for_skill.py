0afa24978e169574ed1ee6f8d42b4cfd6dd2df9c
Lauren Gallic <ldwilli5@ncsu.edu>
2025-09-19 19:38:34 -0400

    cleaned up code

--- certifier_agent/find_endpoints_for_skill.py
+++ certifier_agent/find_endpoints_for_skill.py
@@ -1,16 +1,14 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 # Establish a connection to MongoDB
-
-client = MongoClient('')
-db = client.agent_facts  # Replace with your database name
-collection = db.facts  # Replace with your collection name
+client = MongoClient('your-connection-string') # Replace with your connection string
+db = client.Agents  # Replace with your database name
+collection = db.AgentFacts  # Replace with your collection name
 
 # Define the query criteria
 field_name = "skills.description"  # The field to filter by
-field_value = "Dynamic Pricing"  # The value to match
-field_extract = "endpoints"
+field_value = "aerospace engineering"  # The value to match
+field_extract = "endpoints" # The field you wish to extract
 
 # Query for documents and project only the field_extract field
 # The first argument to find() is the query filter.
@@ -21,7 +19,7 @@ cursor = collection.find({field_name: field_value}, {field_extract: 1})
 # Extract the documents
 ids = [doc[field_extract] for doc in cursor]
 
-# Save the retrieved values
+# Save the retrieved values to a file called urls.txt
 file_name = 'urls.txt'
 with open(file_name, 'w') as file_object:
     for doc_id in ids:
